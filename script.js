91df10b9be06f3843b89a6775dc62779304c54f5
Rendabel Radian <aliqbal345@gmail.com>
2024-11-09 23:35:32 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+document.querySelector('.email-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert("Thank you! We'll reach out when the beta is ready.");
+});
